57b04a49f2fae3f0c91d66109449848a72f68357
Kamil Zielinski <kamilkk.zielinski@gmail.com>
2023-05-30 11:23:26 +0200

    Pass kwargs to generator

--- velocity_estimator/data_generator.py
+++ velocity_estimator/data_generator.py
@@ -255,7 +255,7 @@ def generate(data_path, n_samples, n_frames, batch_size=1000, **kwargs):
     slow_motion_coefs = np.zeros((n_samples, 1))
     generator = TrajectoriesGenerator(min(n_samples, batch_size), n_frames, **kwargs)
     for i in tqdm(range(0, n_samples, batch_size), unit_scale=batch_size):
-        image_positions[i:i+batch_size], image_diameter[i:i+batch_size], velocities[i:i+batch_size], slow_motion_coefs[i:i+batch_size] = generator(add_noise=False)
+        image_positions[i:i+batch_size], image_diameter[i:i+batch_size], velocities[i:i+batch_size], slow_motion_coefs[i:i+batch_size] = generator(**kwargs)
 
     pickle.dump((image_positions, image_diameter, velocities, slow_motion_coefs), open(data_path, 'wb'))
 
